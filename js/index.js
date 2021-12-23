88dcd953bdfeb765b478875f5eb35d43c075f56e
Hero <humphriesa02@gmail.com>
2021-12-23 01:32:04 -0800

    Second Commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  var ti = "AM";
+    if(h > 12){
+        h -= 12;
+        ti = "PM";
+    }
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML =
+  h + ":" + m + ":" + s + " " + ti;
+  var t = setTimeout(startTime, 500);    
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
